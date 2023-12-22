a5abc71f153cd27b0a1b5d485ea47a80a571a642
Administrator <Administrator@C96D15BE24425CD>
2023-12-22 17:32:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Confluent.Kafka;
+using kafka_output;
+
+class Program
+{
+    static void Main()
+    {
+        var config = kafka_output.kafkaConsumerConfig.GetConfig();
+        string topic = "onlineShopping"; // Replace with your topic name
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+        consumer.Subscribe(topic);
+        while (true)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume();
+                Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+
+                // Process the message here
+                consumer.Commit(consumeResult);
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Error consuming message: {e.Error.Reason}");
+            }
+        }
+    }
+}
---
+++ kafkaConsumerConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+namespace kafka_output
+{
+    public static class kafkaConsumerConfig
+    {
+        public static ConsumerConfig GetConfig()
+        {
+            return new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092", // Replace with your Kafka broker address
+                GroupId = "KafkaExampleConsumer",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false,
+            };
+        }
+    }
+}
